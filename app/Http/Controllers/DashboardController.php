e39f2ac4f0c7514e75c65d9883822f06cdfec3e1
CodeWithRibo <riboluna2818@gmail.com>
2025-09-29 21:50:14 +0800

    initial commit

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,28 +15,4 @@ class DashboardController extends Controller
     {
         return view('profile');
     }
-
-    public function create()
-    {
-    }
-
-    public function store(Request $request)
-    {
-    }
-
-    public function show($id)
-    {
-    }
-
-    public function edit($id)
-    {
-    }
-
-    public function update(Request $request, $id)
-    {
-    }
-
-    public function destroy($id)
-    {
-    }
 }
