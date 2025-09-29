e6064590a4480556a567ec179c0c1054915cb4d0
CodeWithRibo <riboluna2818@gmail.com>
2025-09-29 21:51:19 +0800

    added wallet, and expenses routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ExpensesController;
 use App\Http\Controllers\WalletManagementController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,7 +10,8 @@ Route::view('/', 'welcome');
 Route::middleware(['auth', 'is_user', 'verified'])->group(function (){
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('profile', [DashboardController::class, 'profile'])->name('profile');
-    Route::get('wallet', [WalletManagementController::class, 'index'])->name('wallet');
+    Route::get('wallet', WalletManagementController::class)->name('wallet');
+    Route::get('expenses', ExpensesController::class)->name('expenses');
 });
 
 Route::middleware(['auth', 'is_admin'])->group(function (){
