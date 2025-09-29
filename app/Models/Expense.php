10b4369404711a63231c3e3c9dec758ed417cd7f
CodeWithRibo <riboluna2818@gmail.com>
2025-09-29 21:49:28 +0800

    Expense Model and Database

---
+++ app/Models/Expense.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Expense extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'amount',
+        'category',
+        'date',
+        'payment_method',
+        'notes',
+        'wallet_type',
+        'user_id',
+        'wallet_id',
+    ];
+
+    public function User(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function Wallet(): BelongsTo
+    {
+        return $this->belongsTo(Wallet::class);
+    }
+    protected function casts(): array
+    {
+        return [
+            'date' => 'date',
+        ];
+    }
+}
---
+++ database/migrations/2025_09_29_094307_create_expenses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->string('amount');
+            $table->string('category');
+            $table->date('date');
+            $table->string('wallet_type')->nullable();
+            $table->string('payment_method')->nullable();
+            $table->string('notes')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained()
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+
+            $table->foreignId('wallet_id')
+                ->nullable()
+                ->constrained()
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
