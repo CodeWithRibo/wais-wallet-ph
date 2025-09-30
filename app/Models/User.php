bf395ee268cba00da6551a9d3efacb3f7af80295
CodeWithRibo <riboluna2818@gmail.com>
2025-09-30 21:26:05 +0800

    added expense relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,11 @@ class User extends Authenticatable
     {
         return $this->hasMany(Wallet::class);
     }
+
+    public function Expense(): HasMany
+    {
+        return $this->hasMany(Expense::class);
+    }
     /**
      * The attributes that should be hidden for serialization.
      *
