8f9f88742115f5206554bc048853032f94345459
CodeWithRibo <riboluna2818@gmail.com>
2025-10-01 23:57:52 +0800

    Adding sorting

--- app/Livewire/ExpenseTable.php
+++ app/Livewire/ExpenseTable.php
@@ -1,14 +1,40 @@
 <?php
 
 namespace App\Livewire;
+
 use App\Models\Expense;
+use Livewire\Attributes\Url;
 use Livewire\Component;
+use Livewire\WithPagination;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class ExpenseTable extends Component
 {
+    use WithPagination;
+
+    #[Url]
+    public $search = '';
+    #[Url]
+    public $sort = 'created_at';
+    public $sortDirection = 'ASC';
+
+    public function sortBy($field): string
+    {
+        return $this->sort === $field
+
+            ? $this->sortDirection = $this->sortDirection === 'ASC'  ? 'DESC' : 'ASC'
+            : $this->sort = $field;
+    }
+
     public function render()
     {
-        $expenses = Expense::orderBy('created_at', 'ASC')->paginate(10);
+        $sortColumn = $this->sortDirection === 'DESC' ? "-$this->sort" : $this->sort;
+
+        $expenses = QueryBuilder::for(Expense::class)
+            ->allowedSorts(['date', 'category', 'wallet', 'payment_method'])
+            ->defaultSort($sortColumn)
+            ->paginate(10);
+
         return view('livewire.expense-table', compact('expenses'));
     }
 }
