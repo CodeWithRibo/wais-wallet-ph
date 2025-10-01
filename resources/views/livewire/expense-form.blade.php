d14e7e33f987f823a64e2195fe6fc8730a0487e5
CodeWithRibo <riboluna2818@gmail.com>
2025-10-02 00:41:04 +0800

    Adding sorting

--- resources/views/livewire/expense-form.blade.php
+++ resources/views/livewire/expense-form.blade.php
@@ -1,7 +1,6 @@
 <div>
     <form wire:submit="save" method="POST">
         <x-ui.fieldset label="Expenses Information">
-
             <x-ui.field required>
                 <x-ui.label>Amount</x-ui.label>
                 <x-ui.input placeholder="Enter Amount"
@@ -16,9 +15,8 @@
                 <x-ui.select
                     placeholder="Select Category"
                     wire:model="category">
-                    <x-ui.select.option value="personal">Personal</x-ui.select.option>
-                    <x-ui.select.option value="asdad">Personal</x-ui.select.option>
-
+                    <x-ui.select.option value="Transportation">Transportation</x-ui.select.option>
+                    <x-ui.select.option value="HealthCare">HealthCare</x-ui.select.option>
                 </x-ui.select>
                 <x-ui.error name="category"/>
             </x-ui.field>
@@ -29,7 +27,7 @@
                     placeholder="Select Wallet"
                     wire:model="wallet_type">
                     @foreach (auth()->user()->wallet as $wallet)
-                        <x-ui.select.option value="{{strtolower($wallet->wallet_name)}}" icon="wallet">{{$wallet->wallet_name}}</x-ui.select.option>
+                        <x-ui.select.option value="{{strtolower(($wallet->wallet_name))}}" icon="wallet">{{$wallet->wallet_name}}</x-ui.select.option>
                     @endforeach
                 </x-ui.select>
                 <x-ui.error name="wallet_type"/>
@@ -38,20 +36,19 @@
             <x-ui.field required>
                 <x-ui.label>Date</x-ui.label>
                 <x-ui.input type="date"
-                            wire:model="date"
-                            value="<?php echo date('Y-m-d'); ?>"/>
+                            wire:model="date"/>
                 <x-ui.error name="date"/>
             </x-ui.field>
 
-            <x-ui.field required>
+            <x-ui.field>
                 <x-ui.label>Payment Method (Optional)</x-ui.label>
                 <x-ui.select
                     placeholder="Select Payment Method"
                     wire:model="payment_method">
-                    <x-ui.select.option value="cash">Cash</x-ui.select.option>
-                    <x-ui.select.option value="gcash">Gcash</x-ui.select.option>
-                    <x-ui.select.option value="maya">Maya</x-ui.select.option>
-                    <x-ui.select.option value="card">Card</x-ui.select.option>
+                    <x-ui.select.option value="Cash">Cash</x-ui.select.option>
+                    <x-ui.select.option value="Gcash">Gcash</x-ui.select.option>
+                    <x-ui.select.option value="Maya">Maya</x-ui.select.option>
+                    <x-ui.select.option value="Card">Card</x-ui.select.option>
                 </x-ui.select>
             </x-ui.field>
 
@@ -60,10 +57,7 @@
                 <x-ui.input type="text"
                             placeholder="e.g. Buying Big brew"
                             wire:model="notes"/>
-                <x-ui.error name="notes"/>
             </x-ui.field>
-
-            <x-ui.error name="wallet_type"/>
             <div class=" mt-5 flex items-center justify-center">
                 <x-ui.button type="submit">Submit</x-ui.button>
             </div>
--- resources/views/livewire/expense-table.blade.php
+++ resources/views/livewire/expense-table.blade.php
@@ -1,101 +1,157 @@
 <div>
     <div class=" rounded-box border border-base-content/5 bg-base-100 mb-5 p-8">
           <span class="flex items-center gap-2 mb-3">
-                <x-ui.icon  class="size-5" name="funnel"></x-ui.icon>
+                <x-ui.icon class="size-5" name="funnel"></x-ui.icon>
                 <p>Filters & Search</p>
           </span>
         <div class="md:flex items-center space-y-3 md:space-y-0 gap-3">
             <x-ui.input
-                x-model="search"
-                placeholder="Search..."
+                wire:model.live.debounce.300ms="search"
+                placeholder="Search expense.."
                 class="w-full"
                 leftIcon="magnifying-glass"
             />
-{{--TEMPLATE (FIXING SORTING) --}}
+
             <x-ui.select
-                placeholder="Select Wallet"
-                wire:model="wallet_type"
+                placeholder="All Categories"
+                wire:model="#"
                 class="w-full">
-                <x-ui.select.option value="personal" icon="wallet">Personal</x-ui.select.option>
-                <x-ui.select.option value="business" icon="credit-card">Business</x-ui.select.option>
-                <x-ui.select.option value="shared" icon="share">Shared</x-ui.select.option>
+                {{--Need Loop Option--}}
+                <x-ui.select.option value="All Categories">Personal</x-ui.select.option>
+                <x-ui.select.option value="Business">Business</x-ui.select.option>
+                <x-ui.select.option value="Shared">Shared</x-ui.select.option>
             </x-ui.select>
 
             <x-ui.select
-                placeholder="Select Wallet"
-                wire:model="wallet_type"
+                placeholder="All Wallets"
+                wire:model="#"
                 class="w-full">
-                <x-ui.select.option value="personal" icon="wallet">Personal</x-ui.select.option>
-                <x-ui.select.option value="business" icon="credit-card">Business</x-ui.select.option>
-                <x-ui.select.option value="shared" icon="share">Shared</x-ui.select.option>
+                {{--Need Loop Option--}}
+                <x-ui.select.option value="All Wallets">All Wallets</x-ui.select.option>
+                <x-ui.select.option value="business">Business</x-ui.select.option>
+                <x-ui.select.option value="shared">Shared</x-ui.select.option>
             </x-ui.select>
 
-            <x-ui.select
-                placeholder="Select Wallet"
-                wire:model="wallet_type"
-                class="w-full">
-                <x-ui.select.option value="personal" icon="wallet">Personal</x-ui.select.option>
-                <x-ui.select.option value="business" icon="credit-card">Business</x-ui.select.option>
-                <x-ui.select.option value="shared" icon="share">Shared</x-ui.select.option>
-            </x-ui.select>
-
-            <x-ui.select
-                placeholder="Select Wallet"
-                wire:model="wallet_type"
-                class="w-full">
-                <x-ui.select.option value="personal" icon="wallet">Personal</x-ui.select.option>
-                <x-ui.select.option value="business" icon="credit-card">Business</x-ui.select.option>
-                <x-ui.select.option value="shared" icon="share">Shared</x-ui.select.option>
-            </x-ui.select>
         </div>
     </div>
 
-   <div class="rounded-box border border-base-content/5 bg-base-100 overflow-x-auto">
-       <table class="table overflow-x-auto w-full">
-           <thead>
-           <tr class="text-center">
-               <th>Date</th>
-               <th>Amount</th>
-               <th>Category</th>
-               <th>Wallet</th>
-               <th>Payment Method</th>
-               <th>Notes</th>
-           </tr>
-           </thead>
-           <tbody>
-           @foreach($expenses as $index)
-               <tr class="text-center">
-                   <td>{{$index->date->format('Y-m-d')}}</td>
-                   <td class="font-semibold text-[#118139]">₱{{number_format($index->amount, 2)}}</td>
-                   <td>{{ucwords($index->category)}}</td>
-                   <td>
-                       <x-ui.badge size="sm" variant="outline">{{ucwords($index->wallet_type)}}</x-ui.badge>
-                   </td>
-                   <td class="text-center">
-                       @switch($index->payment_method)
-                           @case('cash')
-                               <x-ui.badge color="cash">{{ucfirst($index->payment_method)}}</x-ui.badge>
-                               @break
-                           @case('gcash')
-                               <x-ui.badge color="gcash">{{ucfirst($index->payment_method)}}</x-ui.badge>
-                               @break
-                           @case('maya')
-                               <x-ui.badge color="maya">{{ucfirst($index->payment_method)}}</x-ui.badge>
-                               @break
-                           @case('card')
-                               <x-ui.badge>{{ucfirst($index->payment_method)}}</x-ui.badge>
-                               @break
-                       @endswitch
-                   </td>
-                   <td class="text-start">@if($index->notes == null)
-                           <p class="text-gray-600 font-light">No Additional Notes</p>
-                       @else
-                           {{$index->notes}}
+    <div class="rounded-box border border-base-content/5 bg-base-100 overflow-x-auto">
+        <table class="table overflow-x-auto w-full">
+            <thead>
+            <tr class="text-center">
+                <th>
+                   <span
+                       wire:click="sortBy('date')"
+                       class="inline-flex items-center gap-1 cursor-pointer hover:underline">
+                       <p>Date</p>
+                       @if($sort === 'date')
+                           @if($sortDirection === 'ASC')
+                               <x-ui.icon name="arrow-up" class="size-3 text-gray-500"></x-ui.icon>
+                           @else
+                               <x-ui.icon name="arrow-down" class="size-3 text-gray-500"></x-ui.icon>
+                           @endif
                        @endif
-                   </td>
-               </tr>
-           @endforeach
-           </tbody>
-       </table>
-   </div>
+                   </span>
+                </th>
+                <th>
+                     <span
+                         wire:click="sortBy('amount')"
+                         class="inline-flex items-center gap-1 cursor-pointer hover:underline">
+                       <p>Amount</p>
+                       @if($sort === 'amount')
+                             @if($sortDirection === 'ASC')
+                                 <x-ui.icon name="arrow-up" class="size-3 text-gray-500"></x-ui.icon>
+                             @else
+                                 <x-ui.icon name="arrow-down" class="size-3 text-gray-500"></x-ui.icon>
+                             @endif
+                         @endif
+                   </span>
+                </th>
+                <th>
+                      <span
+                          wire:click="sortBy('category')"
+                          class="inline-flex items-center gap-1 cursor-pointer hover:underline">
+                       <p>Category</p>
+                       @if($sort === 'category')
+                              @if($sortDirection === 'ASC')
+                                  <x-ui.icon name="arrow-up" class="size-3 text-gray-500"></x-ui.icon>
+                              @else
+                                  <x-ui.icon name="arrow-down" class="size-3 text-gray-500"></x-ui.icon>
+                              @endif
+                          @endif
+                   </span>
+                </th>
+                <th>
+                     <span
+                         wire:click="sortBy('wallet_type')"
+                         class="inline-flex items-center gap-1 cursor-pointer hover:underline">
+                       <p>Wallet</p>
+                       @if($sort === 'wallet_type')
+                             @if($sortDirection === 'ASC')
+                                 <x-ui.icon name="arrow-up" class="size-3 text-gray-500"></x-ui.icon>
+                             @else
+                                 <x-ui.icon name="arrow-down" class="size-3 text-gray-500"></x-ui.icon>
+                             @endif
+                         @endif
+                   </span>
+                </th>
+                <th>
+                    <span
+                        wire:click="sortBy('payment_method')"
+                        class="inline-flex items-center gap-1 cursor-pointer hover:underline">
+                       <p>Payment Method</p>
+                       @if($sort === 'payment_method')
+                            @if($sortDirection === 'ASC')
+                                <x-ui.icon name="arrow-up" class="size-3 text-gray-500"></x-ui.icon>
+                            @else
+                                <x-ui.icon name="arrow-down" class="size-3 text-gray-500"></x-ui.icon>
+                            @endif
+                        @endif
+                   </span>
+                </th>
+                <th>Notes</th>
+
+            </tr>
+            </thead>
+            <tbody>
+            @forelse($expenses as $index)
+                <tr class="text-center" wire:key="{{ $index->id }}">
+                    <td>{{$index->date->format('Y-m-d')}}</td>
+                    <td class="font-semibold text-[#118139]">₱{{number_format($index->amount, 2)}}</td>
+                    <td>{{ucwords($index->category)}}</td>
+                    <td>
+                        <x-ui.badge size="sm" variant="outline">{{ucwords($index->wallet_type)}}</x-ui.badge>
+                    </td>
+                    <td class="text-center">
+                        @switch($index->payment_method)
+                            @case('Cash')
+                                <x-ui.badge color="Cash">{{$index->payment_method}}</x-ui.badge>
+                                @break
+                            @case('Gcash')
+                                <x-ui.badge color="Gcash">{{$index->payment_method}}</x-ui.badge>
+                                @break
+                            @case('Maya')
+                                <x-ui.badge color="Maya">{{$index->payment_method}}</x-ui.badge>
+                                @break
+                            @case('Card')
+                                <x-ui.badge>{{$index->payment_method}}</x-ui.badge>
+                                @break
+                        @endswitch
+                    </td>
+                    <td class="text-start">@if($index->notes == null)
+                            <p class="text-gray-600 font-light">No Additional Notes</p>
+                        @else
+                            {{$index->notes}}
+                        @endif
+                    </td>
+                    @empty
+                    <td colspan="6">
+                           <p class="text-center text-2xl text-gray-500"> No Data Found</p>
+                    </td>
+                </tr>
+            @endforelse
+
+            </tbody>
+        </table>
+    </div>
 </div>
