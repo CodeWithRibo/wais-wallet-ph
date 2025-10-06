0633816fdb994a4373da08c3a921bf458c6a5482
CodeWithRibo <riboluna2818@gmail.com>
2025-10-06 21:45:34 +0800

    style: add spacing on button expense delete form

--- resources/views/livewire/expense-delete.blade.php
+++ resources/views/livewire/expense-delete.blade.php
@@ -1,12 +1,12 @@
 <div>
     <form wire:submit.prevent="delete()" method="post">
-        <p>Are you sure want to delete this expense?</p>
-        <div>
+        <p class="pb-2">Are you sure want to delete this expense?</p>
+        <div class="flex items-center justify-end space-x-3">
             <x-ui.button color="slate"
-                         x-on:click="$modal.close('delete-expense')">
+                         x-on:click="$modal.close('delete-expense-{{$id}}')">
                 No
             </x-ui.button>
-            <x-ui.button color="red" type="submit">Yes</x-ui.button>
+            <x-ui.button color="red" type="submit">Yes, Delete it!</x-ui.button>
         </div>
     </form>
 </div>
