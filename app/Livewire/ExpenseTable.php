022a632a68ac7402846701efd314d57af0623cc5
CodeWithRibo <riboluna2818@gmail.com>
2025-10-08 17:52:27 +0800

    feat: add sorting, filtering, and realtime update

--- app/Livewire/ExpenseTable.php
+++ app/Livewire/ExpenseTable.php
@@ -3,7 +3,8 @@
 namespace App\Livewire;
 
 use App\Models\Expense;
-use Database\Seeders\ExpenseSeeder;
+use Illuminate\View\View;
+use Livewire\Attributes\On;
 use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -15,38 +16,94 @@ class ExpenseTable extends Component
 
     #[Url]
     public ?string $search = '';
-    #[Url]
     public $sort = 'created_at';
     public $sortDirection = 'ASC';
-//    public $walletFilter = 'all';
+    public $expense;
+    public $storeExpense;
+    #[Url]
+    public $category_filter = 'All Categories';
+    #[Url]
+    public $wallet_filter = 'All Wallet';
+
+    public function mount(): void
+    {
+        $this->expense = Expense::select(
+            'amount',
+            'category',
+            'date',
+            'wallet_type',
+            'payment_method',
+            'notes')->get();
+    }
+
+    #[On('refresh-table')]
+    public function refresh(): void
+    {
+        $this->resetPage();
+    }
+
+    public function edit($id): void
+    {
+        $this->dispatch('expense-edit', id: $id);
+    }
+
+    public function delete($id): void
+    {
+        $this->dispatch('expense-delete', id: $id);
+    }
+
+    #[On('delete-expense')]
+    public function removeExpense($id): void
+    {
+        $this->expense = $this->expense->reject(fn($expense) => $expense->id === (int)$id);
+    }
+
+    #[On('update-expense')]
+    public function refreshExpense($id): void
+    {
+        $updatedExpense = Expense::findOrFail($id);
+        $this->expense = $this->expense->map(function ($t) use ($updatedExpense) {
+            return $t->id === (int)$updatedExpense->id ? $updatedExpense : $t;
+        });
+    }
 
     public function sortBy($field): string
     {
         return $this->sort === $field
 
-            ? $this->sortDirection = $this->sortDirection === 'ASC'  ? 'DESC' : 'ASC'
+            ? $this->sortDirection = $this->sortDirection === 'ASC' ? 'DESC' : 'ASC'
             : $this->sort = $field;
     }
 
-    public function updatedSearch()
+    public function updatedWalletFilter() : void
     {
         $this->resetPage();
     }
 
+    public function updatedCategoryFilter(): void
+    {
+        $this->resetPage();
+    }
 
-    public function render()
+    public function updatedSearch(): void
+    {
+        $this->resetPage();
+    }
+
+    public function render(): View
     {
         $sortColumn = $this->sortDirection === 'DESC' ? "-$this->sort" : $this->sort;
 
-//        $q = Expense::query();
-//        if (!$this->walletFilter != 'all')
-//            $q->where('wallet_type', $this->walletFilter);
-//
-        $expenses = QueryBuilder::for(Expense::class)
+        $query = QueryBuilder::for(Expense::class)
             ->search(trim($this->search))
             ->allowedSorts(['date', 'category', 'wallet_type', 'payment_method'])
-            ->defaultSort($sortColumn)
-            ->paginate(15);
+            ->defaultSort($sortColumn);
+
+        if ($this->category_filter != 'All Categories') $query->where('category', $this->category_filter);
+        if ($this->wallet_filter != 'All Wallet')  $query->where('wallet_type', $this->wallet_filter);
+
+        $expenses = $query->paginate(15);
+
         return view('livewire.expense-table', compact('expenses'));
     }
 }
