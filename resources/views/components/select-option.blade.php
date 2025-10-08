803080c57df501ec530e51fe8557ba8e98f062dd
CodeWithRibo <riboluna2818@gmail.com>
2025-10-08 17:54:29 +0800

    style : add width & change color

--- resources/views/components/select-option.blade.php
+++ resources/views/components/select-option.blade.php
@@ -3,10 +3,9 @@
 ])
 
 <select {{ $attributes->merge([
-    'class' => 'rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
+    'class' => 'w-full rounded-md shadow-xs border-neutral-300 cursor-pointer focus:border-neutral-300 focus:ring-neutral-300 sm:text-sm'
 ]) }}>
-    <option disabled selected>Select Category</option>
-    @foreach ($options as $value => $label)
+@foreach ($options as $value => $label)
         <option value="{{ $value }}">{{ $label }}</option>
     @endforeach
 </select>
