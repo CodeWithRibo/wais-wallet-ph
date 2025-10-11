f705ad1cf8111c8805cb8871b16c7a28d96faa16
CodeWithRibo <riboluna2818@gmail.com>
2025-10-11 23:41:07 +0800

    chore : organize the default wallets

--- app/Listeners/CreateDefaultWalletsListener.php
+++ app/Listeners/CreateDefaultWalletsListener.php
@@ -9,16 +9,26 @@ class CreateDefaultWalletsListener
 
     public function handle(Registered $event): void
     {
-            $user = $event->user;
+        $user = $event->user;
 
-            $defaultWallets = [
-                ['wallet_name' => 'Personal', 'current_balance' => 1000, 'wallet_type' => 'Personal'],
-                ['wallet_name' => 'Business', 'current_balance' => 1000, 'wallet_type' => 'Business'],
-                ['wallet_name' => 'Shared', 'current_balance' => 1000, 'wallet_type' => 'Shared']
-            ];
+        $defaultWallets = [
+            ['wallet_name' => 'Personal',
+                'current_balance' => 1000,
+                'wallet_type' => 'Personal'
+            ],
+            ['wallet_name' => 'Business',
+                'current_balance' => 1000,
+                'wallet_type' => 'Business'
+            ],
+            ['wallet_name' => 'Shared',
+                'current_balance' => 1000,
+                'wallet_type' => 'Shared'
+            ],
 
-            foreach ($defaultWallets as $wallet){
-                $user->wallet()->create($wallet);
-            }
+        ];
+
+        foreach ($defaultWallets as $wallet) {
+            $user->wallet()->create($wallet);
+        }
     }
 }
