2bb27c9e4566225bf944b4266d943d8eda8510ba
CodeWithRibo <riboluna2818@gmail.com>
2025-10-14 21:30:36 +0800

    feat: add reset monthly spent

--- app/Livewire/WalletManagement.php
+++ app/Livewire/WalletManagement.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\Wallet;
+use Carbon\Carbon;
 use Illuminate\View\View;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -16,36 +17,65 @@ class WalletManagement extends Component
     public float $availBal;
     public float $monthlySpent;
     public $user;
+    public $cycleStart;
+    public $cycleEnd;
 
-    public function wallet()
+
+    public function sumWallet(): void
     {
         $this->user = auth()->user()->load('expense', 'wallet');
-        $expense = $this->user->expense()->sum('amount') ?? 0;
+
+        $firstExpenseDate = $this->user->expense()
+            ->orderBy('created_at')
+            ->value('created_at');
+
+        if ($firstExpenseDate) {
+
+            $start = Carbon::parse($firstExpenseDate)->startOfDay();
+            $today = Carbon::now();
+
+            while ($today->gte($start->copy()->addMonth())) {
+                $start->addMonth();
+            }
+
+            $cycleStart = $start->copy();
+            $cycleEnd = $start->copy()->addMonth();
+
+            $this->monthlySpent = $this->user->expense()
+                ->whereBetween('created_at', [$cycleStart,  $cycleEnd])
+                ->sum('amount');
+
+            $this->cycleStart = $cycleStart;
+            $this->cycleEnd = $cycleEnd;
+
+        } else {
+            $this->monthlySpent = 0;
+        }
+
         $currentBal = $this->user->wallet()->sum('current_balance') ?? 0;
 
-        $available = $currentBal - $expense;
-
         $this->totalBalance = $currentBal;
-        $this->monthlySpent = $expense;
-        $this->availBal = $available;
-
+        $this->availBal = $currentBal - $this->monthlySpent;
 
     }
 
-//    public function walletSummary()
-//    {
-//
-//    }
-
-    public function mount(): void
+    public function edit($id): void
     {
-        $this->wallet();
+        $this->dispatch('edit-wallet', id: $id);
+    }
+
+    #[On(['createWallet', 'refreshEditWallet'])]
+    public function refreshWallet(): void
+    {
+        $this->sumWallet();
     }
 
     public function render(): View
     {
+
         $wallets = $this->user->wallet()
             ->select([
+                'id',
                 'wallet_name',
                 'current_balance',
                 'monthly_spent',
