be1c2da025d73b86ad2945b0170fa56efce02758
CodeWithRibo <riboluna2818@gmail.com>
2025-10-14 20:55:07 +0800

    feat: add wallet edit

---
+++ app/Livewire/WalletEdit.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Wallet;
+use Illuminate\View\View;
+use Livewire\Attributes\On;
+use Livewire\Component;
+
+class WalletEdit extends Component
+{
+    public $wallet_name = '';
+    public $current_balance = '';
+    public $wallet_type = '';
+    public $wallet;
+
+
+    #[On('edit-wallet')]
+    public function loadWallet($id): void
+    {
+
+        $this->wallet = Wallet::query()->findOrFail($id);
+
+        $this->fill($this->wallet->only([
+            'wallet_name',
+            'current_balance',
+            'wallet_type',
+        ]));
+    }
+
+    protected function rules(): array
+    {
+        return [
+            'wallet_name' => 'required',
+            'current_balance' => 'required',
+            'wallet_type' => 'required',
+        ];
+    }
+
+    public function updateWallet(): void
+    {
+
+        $validated = $this->validate();
+        $this->wallet->update([
+            'available_balance' => $this->current_balance,
+            ... $validated
+        ]);
+
+        $this->dispatch('refreshEditWallet');
+        $this->dispatch('close-modal', id: 'edit-wallet-modal');
+    }
+
+    public function render(): View
+    {
+        return view('livewire.wallet-edit');
+    }
+}
---
+++ resources/views/livewire/wallet-edit.blade.php
@@ -0,0 +1,33 @@
+<div>
+    <form wire:submit="updateWallet" method="POST">
+        <x-ui.fieldset label="Wallet Information">
+            <x-ui.field required>
+                <x-ui.label>Wallet</x-ui.label>
+                <x-ui.input placeholder="Enter Wallet Name"
+                            wire:model="wallet_name"/>
+                <x-ui.error name="wallet_name"/>
+
+            </x-ui.field>
+
+            <x-ui.field required>
+                <x-ui.label>Current Balance</x-ui.label>
+                <x-ui.input type="number"
+                            step="0.01"
+                            placeholder="Enter Current Balance"
+                            wire:model="current_balance"/>
+                <x-ui.error name="current_balance"/>
+            </x-ui.field>
+
+            <x-ui.field required>
+                <x-ui.label>Wallet Type</x-ui.label>
+                <x-select-option wire:model="wallet_type"
+                                 :options="['' => 'Select Wallet type','Personal' => 'Personal','Business' => 'Business', 'Shared' => 'Shared']">
+                </x-select-option>
+            </x-ui.field>
+            <x-ui.error name="wallet_type"/>
+            <div class=" mt-5 flex items-center justify-center">
+                <x-ui.button type="submit">Submit</x-ui.button>
+            </div>
+        </x-ui.fieldset>
+    </form>
+</div>
