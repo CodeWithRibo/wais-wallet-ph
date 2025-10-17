1c903b00a200a9559c27c9db0220fd354e1d19da
CodeWithRibo <riboluna2818@gmail.com>
2025-10-17 22:52:31 +0800

    chore: add relationship to category

--- app/Models/User.php
+++ app/Models/User.php
@@ -34,6 +34,11 @@ class User extends Authenticatable
     {
         return $this->hasMany(Expense::class);
     }
+
+    public function category(): HasMany
+    {
+        return $this->hasMany(Category::class);
+    }
     /**
      * The attributes that should be hidden for serialization.
      *
