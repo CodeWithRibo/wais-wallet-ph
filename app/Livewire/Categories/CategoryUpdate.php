48c9730ecd64cd0f7c8930b431b7a1b08a0915be
CodeWithRibo <riboluna2818@gmail.com>
2025-10-18 21:20:51 +0800

    feat : add update category

---
+++ app/Livewire/Categories/CategoryUpdate.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Livewire\Categories;
+
+use App\Models\Category;
+use App\Models\Expense;
+use App\Services\ToastNotificationService;
+use Illuminate\View\View;
+use Livewire\Attributes\On;
+use \Illuminate\Validation\Rule;
+use Livewire\Component;
+
+class CategoryUpdate extends Component
+{
+    public $category_name;
+    public $category_type;
+    public $monthly_budget;
+    public $category;
+
+
+    #[On('update-category')]
+    public function loadCategory($id): void
+    {
+        $this->category = Category::query()->findOrFail($id);
+
+        $this->fill($this->category->only([
+            'category_name',
+            'category_type',
+            'monthly_budget',
+        ]));
+
+        $this->authorize('update', $this->category);
+    }
+
+    protected function rules(): array
+    {
+        return [
+            'category_name' => [
+               'required',
+                Rule::unique('categories')->ignore($this->category->id)
+            ],
+            'category_type' => 'required',
+            'monthly_budget' => 'required',
+        ];
+    }
+
+    public function save(): void
+    {
+        $validated = $this->validate();
+        $data = [
+            'category_name' => $this->category_name,
+            'category_type' => $this->category_type,
+            'monthly_budget' => $this->monthly_budget,
+        ];
+
+        $this->category->fill($data);
+
+        if ($this->category->isDirty()) {
+
+            $expense = Expense::where('category', $this->category_name)->first() ?? 0;
+            $expenseAmount = $expense->amount ?? 0;
+
+            $this->category->update([
+                'remaining' => $this->monthly_budget - $expenseAmount,
+            ]);
+
+            $category = $this->category->save($validated);
+
+            if ($category)
+                $this->dispatch('notify', ... ToastNotificationService::success('Category update successfully'));
+        } else {
+            $this->dispatch('notify', ... ToastNotificationService::info('No changes detected'));
+        }
+
+
+        $this->dispatch('saved-update-category');
+        $this->dispatch('close-modal', id: 'update-category-modal');
+
+    }
+
+    public function render(): View
+    {
+        return view('livewire.categories.category-update');
+    }
+}
