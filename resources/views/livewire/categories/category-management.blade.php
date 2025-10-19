e52a92f9e70b211cbf517c50e39f7f497baf80d3
CodeWithRibo <riboluna2818@gmail.com>
2025-10-19 14:22:31 +0800

    style : change color

--- resources/views/livewire/categories/category-management.blade.php
+++ resources/views/livewire/categories/category-management.blade.php
@@ -38,16 +38,19 @@
                            'Lifestyle' => 'bg-blue-50 text-blue-600',
                            'Work & Business' => 'bg-green-50 text-green-600',
                             'Savings & Investments' => 'bg-yellow-50 text-yellow-600',
-                            'Pet Expenses' => 'bg-rose-50 text-rose-600',
+                             'Pet Expenses' => 'bg-purple-50 text-purple-600',
                             ];
                 @endphp
                 <span class="flex space-x-2 items-center">
-                 <x-ui.badge size="sm" class="{{ $colors[$type] }}"><p class="{{ $colors[$type] }}">{{$type}}</p></x-ui.badge>
+                 <x-ui.badge size="sm" class="{{ $colors[$type] }}"><p
+                         class="{{ $colors[$type] }}">{{$type}}</p></x-ui.badge>
                     <p class="text-sm text-gray-500">({{$group->count()}} categories)</p>
                 </span>
 
                 @foreach($group as $category)
-                 <div class="w-full border border-gray-300 p-4 rounded-lg hover:shadow-md transition-all duration-200" wire:key="{{$category->id}}">
+                    <div
+                        class="w-full border border-gray-300 p-4 rounded-lg hover:shadow-md transition-all duration-200"
+                        wire:key="{{$category->id}}">
                     <span class="flex justify-between items-center pb-5">
                          <p class="text-[15px] text-gray-900">{{$category->category_name}}</p>
                           <x-ui.modal.trigger id="update-category-modal" class="my-4">
@@ -63,21 +66,24 @@
                         <span class="flex space-x-2">
                             <p class="text-[13px] text-gray-500">Budget</p>
                         </span>
-                                <h1 class="text-base font-semibold text-blue-600">₱{{number_format($category->monthly_budget, 2)}}</h1>
+                                <h1 class="text-base font-semibold text-blue-600">
+                                    ₱{{number_format($category->monthly_budget, 2)}}</h1>
                             </div>
 
                             <div class="flex flex-col items-center w-full rounded-lg p-4 bg-red-50">
                         <span class="flex space-x-2">
                             <p class="text-[13px]  text-gray-500">Spent</p>
                         </span>
-                                <h1 class="text-base font-bold text-red-600">₱{{number_format($category->spent, 2)}}</h1>
+                                <h1 class="text-base font-bold text-red-600">
+                                    ₱{{number_format($category->spent, 2)}}</h1>
                             </div>
 
                             <div class="flex flex-col items-center w-full rounded-lg p-4 bg-green-50">
                         <span class="flex space-x-2">
                             <p class="text-[13px]  text-gray-500">Remaining</p>
                         </span>
-                                <h1 class="text-base font-bold text-green-600">₱{{number_format($category->remaining, 2)}}</h1>
+                                <h1 class="text-base font-bold text-green-600">
+                                    ₱{{number_format($category->remaining, 2)}}</h1>
                             </div>
 
                         </div>
