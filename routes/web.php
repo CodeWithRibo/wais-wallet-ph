829ec20a41d2bcefa0831231ee46f9e23a61b007
CodeWithRibo <riboluna2818@gmail.com>
2025-11-09 14:15:10 +0800

    chore : added logout url

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,9 @@ use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ExpensesController;
 use App\Http\Controllers\WalletManagementController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
 
 Route::view('/', 'welcome')->name('welcome');
 
@@ -15,13 +17,20 @@ Route::middleware(['auth', 'is_user', 'verified'])->group(function (){
     Route::get('wallet', WalletManagementController::class)->name('wallet');
     Route::get('expenses', ExpensesController::class)->name('expenses');
     Route::get('categories', CategoriesController::class)->name('categories');
-    Route::delete('logout',LogoutController::class)->name('logout-account');
 });
 
 Route::middleware(['auth', 'is_admin'])->group(function (){
    Route::get('admin/dashboard', function (){
        return 'admin mode';
    })->name('admin.dashboard');
+});
+
+Route::delete('logout',LogoutController::class)->name('logout-account');
+Route::get('/logout', function () {
+    Auth::guard('web')->logout();
+    Session::invalidate();
+    Session::regenerateToken();
+    return redirect()->route('welcome');
 });
 
 require __DIR__.'/auth.php';
