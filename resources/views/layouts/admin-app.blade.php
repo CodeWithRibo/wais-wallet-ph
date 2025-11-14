80e5609fe6e8244ceade5387a9fb869ba7f2ddda
CodeWithRibo <riboluna2818@gmail.com>
2025-11-14 15:27:24 +0800

    chore : added admin layout

---
+++ resources/views/layouts/admin-app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="light">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" >
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Wais Wallet PH') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="font-sans antialiased">
+<div class="min-h-screen bg-gray-100">
+    <livewire:layout.navigation/>
+
+    <!-- Page Content -->
+    <main>
+        @yield('content')
+        <x-ui.toast maxToasts="5" />
+    </main>
+</div>
+@livewireScripts
+</body>
+</html>
