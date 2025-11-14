8e912a2e9788d4bd4849a4f8aa799b73ab77ad99
CodeWithRibo <riboluna2818@gmail.com>
2025-11-14 14:24:40 +0800

    chore: added logo

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -36,7 +36,7 @@ class extends Component {
     <form wire:submit="login">
         <div class="m-5 block text-center">
             <span class="flex items-center justify-center">
-                      <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                      <x-application-logo class="w-20 h-20 fill-current text-gray-500"/>
                 <h1 class="text-2xl sm:text-3xl">Wait Wallet PH</h1>
             </span>
             <p class="text-gray-500 text-sm">Sign in to your account</p>
@@ -61,14 +61,16 @@ class extends Component {
                           class="block mt-1 w-full focus:border-green-400 focus:ring-green-400"
                           type="password"
                           name="password"
-                          required autocomplete="current-password"
+                          required autocomplee="current-password"
                           placeholder="******"/>
 
             <x-input-error :messages="$errors->get('form.password')" class="mt-2"/>
         </div>
 
         <div class="mt-5">
-            <button class="cursor-pointer py-[8px] text-white w-full bg-emerald-500 hover:bg-emerald-600 transition-all duration-300 border-none rounded-md" type="submit">
+            <button
+                class="cursor-pointer py-[8px] text-white w-full bg-emerald-500 hover:bg-emerald-600 transition-all duration-300 border-none rounded-md"
+                type="submit">
                 {{ __('Log in') }}
             </button>
         </div>
@@ -84,7 +86,8 @@ class extends Component {
 
         <div class="flex items-center justify-center space-x-2 mt-5">
             <h1 class="text-base text-gray-500">Don't have an account? </h1>
-            <a href="{{route('register')}}" class="text-sm text-emerald-500 hover:text-emerald-600 hover:underline">Sign up now</a>
+            <a href="{{route('register')}}" class="text-sm text-emerald-500 hover:text-emerald-600 hover:underline">Sign
+                up now</a>
         </div>
     </form>
 </div>
