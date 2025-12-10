0150b2fba85682a3843c6292ab23285924657bab
CodeWithRibo <riboluna2818@gmail.com>
2025-12-10 13:00:15 +0800

    chore: remove default wallet

--- app/Listeners/CreateDefaultWalletsListener.php
+++ app/Listeners/CreateDefaultWalletsListener.php
@@ -7,37 +7,37 @@ use Illuminate\Auth\Events\Registered;
 class CreateDefaultWalletsListener
 {
 
-    public function handle(Registered $event): void
-    {
-        $user = $event->user;
-
-        $defaultWallets = [
-            ['wallet_name' => 'Personal',
-                'current_balance' => 1000,
-                'monthly_spent' => 0,
-                'transaction' => 0,
-                'available_balance' => 1000,
-                'wallet_type' => 'Personal'
-            ],
-            ['wallet_name' => 'Business',
-                'current_balance' => 1000,
-                'monthly_spent' => 0,
-                'transaction' => 0,
-                'available_balance' => 1000,
-                'wallet_type' => 'Business'
-            ],
-            ['wallet_name' => 'Shared',
-                'current_balance' => 1000,
-                'monthly_spent' => 0,
-                'transaction' => 0,
-                'available_balance' => 1000,
-                'wallet_type' => 'Shared'
-            ],
-
-        ];
-
-        foreach ($defaultWallets as $wallet) {
-            $user->wallet()->create($wallet);
-        }
-    }
+//    public function handle(Registered $event): void
+//    {
+//        $user = $event->user;
+//
+//        $defaultWallets = [
+//            ['wallet_name' => 'Personal',
+//                'current_balance' => 0,
+//                'monthly_spent' => 0,
+//                'transaction' => 0,
+//                'available_balance' => 0,
+//                'wallet_type' => 'Personal'
+//            ],
+//            ['wallet_name' => 'Business',
+//                'current_balance' => 0,
+//                'monthly_spent' => 0,
+//                'transaction' => 0,
+//                'available_balance' => 0,
+//                'wallet_type' => 'Business'
+//            ],
+//            ['wallet_name' => 'Shared',
+//                'current_balance' => 0,
+//                'monthly_spent' => 0,
+//                'transaction' => 0,
+//                'available_balance' => 0,
+//                'wallet_type' => 'Shared'
+//            ],
+//
+//        ];
+//
+//        foreach ($defaultWallets as $wallet) {
+//            $user->wallet()->create($wallet);
+//        }
+//    }
 }
